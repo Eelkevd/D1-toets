3de3ea57315d970f7ac9bd1ca40eb6ca69affbf1
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-01-26 12:08:53 +0100

    Versie 4
    verwijzing klopt nu

--- d1 toets.php
+++ d1 toets.php
@@ -114,7 +114,7 @@ $cols = 6;
 <head>
 </head>
 <body>
-	<form action="d1 toets extra" method="get">
+	<form action="d1 toets extra.php" method="get">
 		<input type="text" name="width" placeholder="width">
 		<input type="text" name="height" placeholder="height">
 		<input type="submit"  name="submit" value="Submit" />
